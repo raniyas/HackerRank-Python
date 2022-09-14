de4f1d2a9c433b7d0a3fd88d6b9a983674eada82
raniyas <36111742+raniyas@users.noreply.github.com>
2022-09-14 21:46:10 +0530

    Create division.py

---
+++ division.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    quotient_int = a//b
+    print(quotient_int)
+    
+    quotient_float = a/b
+    print(quotient_float)
